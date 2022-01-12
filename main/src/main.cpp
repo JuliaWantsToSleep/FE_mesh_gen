9a6931f12d1588c639a6836f50182080560003b3
agat <agat@VirtualDebian10.01050-1.ru>
2022-01-12 22:54:59 +0300

    [upd] Добавлен код запуска алгоритмов

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
 
+#include "Solution.hpp"
 #include "StepTranslator.hpp"
+#include "EllipsePackingMethod.hpp"
+
 
 int main(int argc, char* argv[])
 {
-    std::vector<std::shared_ptr<BSplineSurface>> surf_vec;
-    StepTranslator tr;
-    tr.Translate("for_parser.step", surf_vec);
-    return 0;
+    std::string input;
+    if (argc == 0)
+    {
+        std::cout << "Starting with default surface\n";
+        input = StepTranslator::GetDefaultPath();
+    }
+    else
+    {
+        input = argv[0];
+        if (input.empty())
+        {
+            std::cerr << "Invalid input.\n";
+            return INPUT_ERR;
+        }
+    }
+    std::shared_ptr<StepTranslator> stp_translator =
+            std::make_shared<StepTranslator>(input);
+    std::shared_ptr<EllipsePackingMethod> pack_method =
+            std::make_shared<EllipsePackingMethod>();
+    Solution sol{stp_translator, pack_method};
+    auto ret_code = sol.Solve();
+
+
+    return ret_code;
 }
